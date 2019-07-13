b5ebcb48285a6f3c3df25dc764085a9c730acf12
jony5ds <37637355+jony5ds@users.noreply.github.com>
2019-07-13 20:58:25 -0300

    Salvando email do usário e senha
    Utiliando firebase, realizei o cadastro do email e senha do usuário

--- WhatsClone/app/src/main/java/com/example/whatsclone/activity/cadastro/CadastroUsuarioActivity.java
+++ WhatsClone/app/src/main/java/com/example/whatsclone/activity/cadastro/CadastroUsuarioActivity.java
@@ -2,20 +2,28 @@ package com.example.whatsclone.activity.cadastro;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.whatsclone.R;
+import com.example.whatsclone.config.ConfiguracaoFirebase;
 import com.example.whatsclone.databinding.ActivityCadastroUsuarioBinding;
 import com.example.whatsclone.obj.UsuarioObj;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
 
   private static ActivityCadastroUsuarioBinding mBinding;
     UsuarioObj mUsuario;
+    FirebaseAuth autentica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,18 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
     }
 
     private void cadastrarUsuario() {
+        autentica = ConfiguracaoFirebase.getAutenticacao();
+        autentica.createUserWithEmailAndPassword(
+                mUsuario.getEmail(),mUsuario.getSenha()
+        ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                    Toast.makeText(getApplicationContext(),getString(R.string.sucesso_cadastro),Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(),getString(R.string.erro_envio),Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
--- WhatsClone/app/src/main/java/com/example/whatsclone/config/ConfiguracaoFirebase.java
+++ WhatsClone/app/src/main/java/com/example/whatsclone/config/ConfiguracaoFirebase.java
@@ -1,11 +1,13 @@
 package com.example.whatsclone.config;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public final class ConfiguracaoFirebase {
 
     private static DatabaseReference referenciaFireBase;
+    private static FirebaseAuth auth;
 
     public static DatabaseReference getFirebase() {
 
@@ -13,5 +15,12 @@ public final class ConfiguracaoFirebase {
          referenciaFireBase  = FirebaseDatabase.getInstance().getReference();
 
         return referenciaFireBase;
+    }
+
+    public static FirebaseAuth getAutenticacao()
+    {
+        if(auth == null)
+            auth = FirebaseAuth.getInstance();
+        return auth;
     }
 }
