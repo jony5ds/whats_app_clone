506f3086da5db9031c9f3d43de32221463e8871b
jony5ds <37637355+jony5ds@users.noreply.github.com>
2019-07-24 00:15:43 -0300

    Criando método de logar
    Criei o método para logar o usuário, mas ainda falta criar a activity na qual será chamada

--- WhatsClone/app/src/main/java/com/example/whatsclone/activity/login/LoginActivity.java
+++ WhatsClone/app/src/main/java/com/example/whatsclone/activity/login/LoginActivity.java
@@ -1,17 +1,10 @@
 package com.example.whatsclone.activity.login;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.SmsManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -20,28 +13,74 @@ import com.example.whatsclone.activity.cadastro.CadastroUsuarioActivity;
 import com.example.whatsclone.config.ConfiguracaoFirebase;
 import com.example.whatsclone.obj.UsuarioObj;
 import com.example.whatsclone.databinding.ActivityLoginBinding;
-import com.example.whatsclone.helper.Permissoes;
-import com.example.whatsclone.helper.Preferencias;
-import com.google.firebase.database.DatabaseReference;
-
-import java.util.HashMap;
-import java.util.Random;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private static ActivityLoginBinding mBinding;
-    private UsuarioObj user;
-    private String[] permissoes = new String[]{
-            Manifest.permission.SEND_SMS,
-            Manifest.permission.INTERNET
-    };
+    private UsuarioObj mUser;
+    private FirebaseAuth mAuth;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        mAuth = ConfiguracaoFirebase.getAutenticacao();
 
+        verificarUsuarioLogado();
+
+        mUser = new UsuarioObj();
+
+        mBinding.btnLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Primeiro passo inputar o valores digitados pelo usuário no objeto
+                obterUsuario();
+
+                // Segundo passo validar o que foi digitado pelo usuário no banco
+                validarLogin();
+            }
+        });
+
+
+    }
+
+    private void verificarUsuarioLogado() {
+       if (mAuth.getCurrentUser() != null)
+       {
+
+       }
+
+
+    }
+
+    private void validarLogin() {
+
+        mAuth.signInWithEmailAndPassword(
+                mUser.getEmail(),
+                mUser.getSenha()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(LoginActivity.this,"Login efetuado com sucesso!",Toast.LENGTH_SHORT).show();
+                } else
+                {
+                    Toast.makeText(LoginActivity.this,"Erro ao efetuar login!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void obterUsuario() {
+        mUser.setEmail(mBinding.edtEmail.getText().toString());
+        mUser.setSenha(mBinding.edtSenha.getText().toString());
 
     }
 
