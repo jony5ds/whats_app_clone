b711e66ba8213273d560150a406b0146ad793845
jony5ds <37637355+jony5ds@users.noreply.github.com>
2019-08-02 21:38:09 -0300

    tab_layout e fragment
    Implementando tab_layout do git hub que é mais atualizado que a plataforma android.
    Criei fragment conversas e fragment contatos

---
+++ WhatsClone/app/src/main/java/com/example/whatsclone/activity/conversas/ConversasFragment.java
@@ -0,0 +1,33 @@
+package com.example.whatsclone.activity.conversas;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.whatsclone.R;
+
+/**
+ * Criado por Jônatas Barbosa
+ * em 02/08/2019
+ */
+public class ConversasFragment extends Fragment {
+
+    public ConversasFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_conversas, container, false);
+    }
+
+
+
+
+}
