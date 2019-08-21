6285dd615efc8c6a4c88496172d09acc04d27212
jony5ds <37637355+jony5ds@users.noreply.github.com>
2019-08-20 23:05:16 -0300

    Listando contatos do firebase

--- WhatsClone/app/src/main/java/com/example/whatsclone/activity/conversas/ContatoFragment.java
+++ WhatsClone/app/src/main/java/com/example/whatsclone/activity/conversas/ContatoFragment.java
@@ -2,6 +2,7 @@ package com.example.whatsclone.activity.conversas;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +10,14 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import com.example.whatsclone.R;
+import com.example.whatsclone.config.ConfiguracaoFirebase;
 import com.example.whatsclone.databinding.FragmentContatoBinding;
+import com.example.whatsclone.helper.Preferencias;
+import com.example.whatsclone.obj.ContatoObj;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -19,9 +27,23 @@ public class ContatoFragment extends Fragment {
     private FragmentContatoBinding mBinding;
     private ArrayAdapter mAdapter;
     private ArrayList<String> mContatos;
+    private DatabaseReference mFirebase;
+    private ValueEventListener mValueEventListenerContatos;
 
     public ContatoFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mFirebase.removeEventListener(mValueEventListenerContatos);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
     }
 
     @Override
@@ -31,17 +53,38 @@ public class ContatoFragment extends Fragment {
         //Intanciar objetos
         mContatos = new ArrayList<>();
 
-        //Teste
-        mContatos.add("Maria Julia");
-        mContatos.add("Ana Clara");
-        mContatos.add("Jeferson Silva");
-        mContatos.add("Rodolpho Oliveira");
-        mContatos.add("Ewerton Ribeiro");
-
         //Monta listView e adapter
+        Preferencias preferencias = new Preferencias(getActivity());
+        String usuario = preferencias.getIdentificador();
         mAdapter = new ArrayAdapter(getActivity(),R.layout.lista_contato,mContatos);
 
         mBinding.listaContatos.setAdapter(mAdapter);
+
+        //recuperar dados do firebase
+        mFirebase = ConfiguracaoFirebase.getFirebase()
+                    .child("contatos")
+                    .child(usuario);
+        mValueEventListenerContatos = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                // limpar contatos
+                mContatos.clear();
+                //listar contatos
+                for(DataSnapshot data : dataSnapshot.getChildren())
+                {
+                    ContatoObj contato = data.getValue(ContatoObj.class);
+                    mContatos.add(contato.getNome());
+                    mAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        mFirebase.addValueEventListener(mValueEventListenerContatos);
 
         return mBinding.getRoot();
     }
