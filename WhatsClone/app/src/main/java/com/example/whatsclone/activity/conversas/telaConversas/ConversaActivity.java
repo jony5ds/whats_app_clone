174c9b69b77d8fa2bdfbc28bd915991c17e095f2
jony5ds <37637355+jony5ds@users.noreply.github.com>
2019-08-25 01:00:04 -0300

    Layout da tela de conversas

--- WhatsClone/app/src/main/java/com/example/whatsclone/activity/conversas/telaConversas/ConversaActivity.java
+++ WhatsClone/app/src/main/java/com/example/whatsclone/activity/conversas/telaConversas/ConversaActivity.java
@@ -1,15 +1,37 @@
 package com.example.whatsclone.activity.conversas.telaConversas;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.whatsclone.R;
+import com.example.whatsclone.databinding.ActivityConversasBinding;
+import com.example.whatsclone.databinding.TelaConversaBinding;
 
 public class ConversaActivity extends AppCompatActivity {
+
+    private TelaConversaBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_conversa);
+        mBinding = DataBindingUtil.setContentView(this,R.layout.tela_conversa);
+
+        mBinding.toolbarConversa.toolbarPrincipal.setTitle("Usuario");
+        setSupportActionBar(mBinding.toolbarConversa.toolbarPrincipal);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        mBinding.toolbarConversa.toolbarPrincipal.setNavigationOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                ConversaActivity.this.finish();
+            }
+
+        });
+
+
     }
 }
